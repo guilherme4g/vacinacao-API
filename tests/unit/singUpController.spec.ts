4e954001c21c23ef556fd67d481093910b253e60
jose guilherme <jgnvs6@gmail.com>
2021-04-04 19:28:00 -0300

    test: ensure SingUpController return 400 if any field is invalid

--- tests/unit/singUpController.spec.ts
+++ tests/unit/singUpController.spec.ts
@@ -50,4 +50,21 @@ describe('Person Controller', () => {
     expect(httpResponse.status).toBe(500);
     expect(httpResponse.body).toContain('Internal Server Error');
   });
+
+  test('Should return 400 if any field is incorrecty', async () => {
+    const { sut, singUpvalidatorStub } = makeSut();
+    const validationSpy = jest.spyOn(singUpvalidatorStub, "validation");
+    validationSpy.mockReturnValueOnce([ `invalid param: 'cpf'`  ]);
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        cpf: 'invalid_cpf',
+        age: 27,
+        email: 'any_email'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.status).toBe(400);
+    expect(httpResponse.body).toContain("invalid param: 'cpf'");
+  });
 });
