f3c674705be9bb1d99da615da718cdbda1e8eebd
jose guilherme <jgnvs6@gmail.com>
2021-04-04 20:07:28 -0300

    test: ensure addPerson in SignUpController get correct values

--- tests/unit/singUpController.spec.ts
+++ tests/unit/singUpController.spec.ts
@@ -1,13 +1,15 @@
 import { SignUpPersonController } from "../../src/controllers/SignUp/SignUpController";
 import { SingUpvalidator } from "../../src/controllers/SignUp/SignUpValidator";
-
+import { AddPersonRepository } from "../../src/repositories/fake/AddPersonRepository";
 
 // sut => system under test => sistema em teste
 const makeSut = () => {
   const singUpvalidatorStub = new SingUpvalidator();
-  const sut = new SignUpPersonController(singUpvalidatorStub);
+  const addPersonRepositoryStub = new AddPersonRepository();
+  const sut = new SignUpPersonController(singUpvalidatorStub, addPersonRepositoryStub);
   return {
     singUpvalidatorStub,
+    addPersonRepositoryStub,
     sut
   };
 }
@@ -66,5 +68,25 @@ describe('Person Controller', () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.status).toBe(400);
     expect(httpResponse.body).toContain("invalid param: 'cpf'");
+  });
+
+  test('Should call addPerson with correct values', async () => {
+    const { sut, addPersonRepositoryStub } = makeSut();
+    const addPersonSpy = jest.spyOn(addPersonRepositoryStub, "add");
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        cpf: 'any_cpf',
+        age: 27,
+        email: 'any_email'
+      }
+    }
+    await sut.handle(httpRequest);
+    expect(addPersonSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      cpf: 'any_cpf',
+      age: 27,
+      email: 'any_email'
+    });
   });
 });
